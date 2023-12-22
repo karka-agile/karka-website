3e21d9e8583f7c730d884d062eac6dce6e6478a9
sivaranjani24 <sivaranjani@inbox.flowglobal.net>
2023-12-22 12:42:05 +0530

    Changes in the landing page

--- include/footer.php
+++ include/footer.php
@@ -5,7 +5,7 @@
         </div>
         <div class="col-lg-6 col-md-12 text-sm-center text-md-center text-lg-center text-center">
             <a href="index.html" target="_self" class="text-decoration-none">Home</a>            
-            <a href="about.htl" target="_self" class="text-decoration-none">About</a>            
+            <a href="about.html" target="_self" class="text-decoration-none">About</a>            
             <a href="fullstack.html" target="_self" class="text-decoration-none">Fullstack</a>            
             <a href="fees_pap.html" target="_self" class="text-decoration-none">Fees & PAPP</a>            
             <button type='button' class='get-in mt-3 sm-mt-0' onclick="window.open('https://karka.academy/apply/', '_blank')">Apply now</button>        
